847fdc42c857c87e96fff3e7d13f80135662a3c5
Luke Wilde <luke.wilde@gmail.com>
2015-05-27 10:20:40 +0100

    Adjusting properties.

--- src/js/game/properties.js
+++ src/js/game/properties.js
@@ -1,7 +1,7 @@
 var npmProperties = require('../../../package.json');
 
 module.exports =
-  { title: 'Phaser JS Boilerplate'
+  { title: 'Resonant Waves'
   , description: npmProperties.description
   , port: 3017
   , liveReloadPort: 3018
